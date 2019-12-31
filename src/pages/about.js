77ba04e6c7f95e7e397b048cf589286d18c80b55
Ryan Whittingham <whittingham777@gmail.com>
2019-12-31 15:31:31 -0800

    MVP

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Head from "../components/head"
+
 
 const AboutPage = () => {
   return (
     <Layout>
+      <Head title="About" />
       <h1>About</h1>
       <p>This is my about page. How about that.</p>
       <p>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,13 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import Head from "../components/head"
+
 
 const ContactPage = () => {
   return (
     <Layout>
+      <Head title="Contact" />
       <h1>Contact</h1>
       <p>This is my contact page. 3..2..1..Contact.</p>
       <a href="https://google.com" target="_blank">
